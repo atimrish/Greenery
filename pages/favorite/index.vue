4cb53b5b4cf110ab3b2f247acd47f6a09a14fd13
atimrish <atimrish11@gmail.com>
2023-06-19 04:34:59 +0400

    f

--- pages/favorite/index.vue
+++ pages/favorite/index.vue
@@ -8,7 +8,7 @@ useHead({
 </script>
 
 <template>
-  <div class="xl:w-[1200px] m-auto">
+  <div class="xl:w-[1200px] m-auto min-h-screen">
     <h2 class="text-[24px] mb-24">Избранное</h2>
     <FavoriteContainer/>
   </div>
